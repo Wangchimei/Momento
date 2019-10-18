bec320a4f8eac773d6bdde35da67e104df3fec5f
Chi-Mei Wang <kimmy0929@gmail.com>
2019-10-18 16:46:17 +0900

    set up devise setting

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,15 @@
 Rails.application.routes.draw do
+  if Rails.env.development?
+    mount LetterOpenerWeb::Engine, at: "/letter_opener"
+  end
+
+  root 'feeds#index'
+
   resources :feeds do
     collection do
       post :confirm
     end
   end
+
+  devise_for :users
 end
