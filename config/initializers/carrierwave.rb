4535207932d12a5ae761f173e1fbcb6f2a90d399
Chi-Mei Wang <kimmy0929@gmail.com>
2019-11-10 16:28:31 +0900

    modify carrierwave config

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -13,15 +13,27 @@ CarrierWave.configure do |config|
       path_style:            true,
   }
 
+  # 公開・非公開の切り替え
   config.fog_public     = true
+  # キャッシュの保存期間
   config.fog_attributes = {'Cache-Control' => 'public, max-age=86400'}
 
+  # キャッシュをS3に保存
+  # config.cache_storage = :fog
+
+  # 環境ごとにS3のバケットを指定
   case Rails.env
     when 'production'
       config.fog_directory = 'momento-production'
-      config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/momento-production'
+      config.asset_host = 'https://momento-production.s3-ap-northeast-1.amazonaws.com'
     when 'development'
       config.fog_directory = 'momento-development'
-      config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/momento-development'
+      config.asset_host = 'https://momento-development.s3-ap-northeast-1.amazonaws.com'
+    # when 'test'
+    #   config.fog_directory = 'momento-test'
+    #   config.asset_host = 'https://momento-test.s3-ap-northeast-1.amazonaws.com'
   end
 end
+
+# 日本語ファイル名の設定
+CarrierWave::SanitizedFile.sanitize_regexp = /[^[:word:]\.\-\+]/
